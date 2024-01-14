8658c2fcac197d38d1cd60e4b4d4aad246e34ef9
Hy <hyhuynh222@gmail.com>
2024-01-13 22:06:40 -0800

    add: Deploy:

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/hyah01.github.io/",
+  base: "/profolio-project/",
 })
