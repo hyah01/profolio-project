2e3dfcf81708f57b5d4eec5b9aa7054179e58989
Hy <hyhuynh222@gmail.com>
2024-01-14 11:18:18 -0800

    add: new domain

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/profolio-project/",
+  base: "/hyah01.github.io/",
 })
