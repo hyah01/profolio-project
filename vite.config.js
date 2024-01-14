eaef2f71e0379c8387186e909a36db5d80ea63e7
Hy <hyhuynh222@gmail.com>
2024-01-13 21:52:24 -0800

    add: deploy workflows

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/hyah01.github.io",
   plugins: [react()],
+  base: "/hyah01.github.io/",
 })
